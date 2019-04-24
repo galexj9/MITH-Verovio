c057199546de7a3fa050ec07676f8a372e9d7bb3
galexj9 <43096683+galexj9@users.noreply.github.com>
2019-04-23 21:06:53 -0400

    centering
    created script.js again and centered the verovio viewer

---
+++ script.js
@@ -0,0 +1,12 @@
+import 'https://www.verovio.org/javascript/app/verovio-app.js';
+// Create the app - here with an empty option object
+const app = new Verovio.App(document.getElementById("app"), {});
+// Load a file (MEI or MusicXML)
+//temporarily disabled so the website loads faster while working on it
+/**
+fetch("https://www.verovio.org/editor/brahms.mei")
+    .then(function(response) {
+      return response.text();
+    }).then(function(text) {
+        app.loadData(text);
+    }); **/
