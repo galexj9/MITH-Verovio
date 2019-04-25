253b53c735e487364d1d2eac66ca9391f9edeb6a
galexj9 <43096683+galexj9@users.noreply.github.com>
2019-04-25 08:28:40 -0400

    downloaded verovio pt2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-import verovio.js;
+import "verovio.js";
 // Create the app - here with an empty option object
 const app = new Verovio.App(document.getElementById("app"), {});
 // Load a file (MEI or MusicXML)
 //temporarily disabled so the website loads faster while working on it
-
+/**
 fetch("https://www.verovio.org/editor/brahms.mei")
     .then(function(response) {
       return response.text();
     }).then(function(text) {
-        app.loadData(text);
-    });
+      app.loadData(text);
+    }); **/
