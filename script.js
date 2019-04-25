c0ea510b73613648e4efa89b94ca1a6af2bdd614
galexj9 <43096683+galexj9@users.noreply.github.com>
2019-04-25 08:20:54 -0400

    Downloaded verovio.js
    allows for offline useage and prevents CORS request violations on chrome thru atom.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import 'https://www.verovio.org/javascript/app/verovio-app.js';
+import verovio.js;
 // Create the app - here with an empty option object
 const app = new Verovio.App(document.getElementById("app"), {});
 // Load a file (MEI or MusicXML)
@@ -9,4 +9,4 @@ fetch("https://www.verovio.org/editor/brahms.mei")
       return response.text();
     }).then(function(text) {
         app.loadData(text);
-    }); 
+    });
