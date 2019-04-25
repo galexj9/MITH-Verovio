954a322c847b9248dc7b58c2dd44362fedaa3b2d
galexj9 <43096683+galexj9@users.noreply.github.com>
2019-04-25 08:11:23 -0400

    uncommented file loader

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ import 'https://www.verovio.org/javascript/app/verovio-app.js';
 const app = new Verovio.App(document.getElementById("app"), {});
 // Load a file (MEI or MusicXML)
 //temporarily disabled so the website loads faster while working on it
-/**
+
 fetch("https://www.verovio.org/editor/brahms.mei")
     .then(function(response) {
       return response.text();
     }).then(function(text) {
         app.loadData(text);
-    }); **/
+    }); 
