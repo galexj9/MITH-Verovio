140d9e31c96ec01bc0a2b03496c96356047e10b8
galexj9 <43096683+galexj9@users.noreply.github.com>
2019-05-09 21:29:12 -0400

    Changed to JS Lib
    Changed everything over to a JS library.
    Added "Pinter's Additions" button which right now just makes the pedal markings red.

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
-//import './lib/verovio.js';
-// Create the app - here with an empty option object
-const app = new Verovio.App(document.getElementById("app"), {});
-// Load a file (MEI or MusicXML)
-//temporarily disabled so the website loads faster while working on it
+/* Some global variables */
+var vrvToolkit = new verovio.toolkit();
+var zoom = 50;
+//defaults
+var pageHeight = 1920;
+var pageWidth = 1080;
 
-fetch("./data/Nocturne72EditedV3.mei")
-    .then(function(response) {
-      return response.text();
-    }).then(function(text) {
-      app.loadData(text);
-    });
 
-//promises
-//fetching or redawing
+/* A function for setting options to the toolkit */
+function setOptions() {
+  /* Adjust the height and width according to the window size */
+  pageHeight = document.pageHeight;
+  pageWidth = document.pageWidth;
+  options = {
+    pageHeight: pageHeight,
+    pageWidth: pageWidth,
+    scale: zoom,
+    adjustPageHeight: true
+  };
+  vrvToolkit.setOptions(options);
+}
+
+/* A function that sets the options, loads the data and render the first page */
+function loadData(data) {
+  setOptions();
+  vrvToolkit.loadData(data);
+  svg = vrvToolkit.renderToSVG(1, {});
+  $("#output").html(svg);
+}
+
+/* a function that paints the pedal markings all red */
+function paintPeds() {
+  var style = window.getComputedStyle($(".pedal")[0]);
+  var red = "rgb(200, 0, 0)", black = "rgb(0, 0, 0)";
+  //checks the first pedal's style
+  if (style.stroke == red) {
+    $(".pedal").attr("fill", black).attr("stroke", black);
+  } else {
+    $(".pedal").attr("fill", red).attr("stroke", red);
+  }
+
+}
+
+$(document).ready(function() {
+  //verovio file loading
+  var file = "/data/Nocturne72EditedV3C.mei";
+  $.ajax({
+    url: file,
+    dataType: "text",
+    success: function(data) {
+      loadData(data);
+    }
+  });
+
+  //toggle button
+  $('a#button').click(function() {
+    $(this).toggleClass("down");
+    paintPeds();
+  });
+});
